f1ae6228567c5c7d649fc9242a9f41a319e195e3
Mike Verbanic <verbanicm@users.noreply.github.com>
2023-02-01 09:13:59 -0500

    add integration tests (#10)
    This is a quick, first pass at getting SOMETHING working for integration
    tests.

--- go.mod
+++ go.mod
@@ -3,28 +3,37 @@ module github.com/abcxyz/github-metrics-aggregator
 go 1.19
 
 require (
+	cloud.google.com/go/bigquery v1.45.0
 	cloud.google.com/go/pubsub v1.28.0
-	github.com/abcxyz/pkg v0.1.1
-	github.com/google/go-github/v48 v48.2.0
+	github.com/abcxyz/pkg v0.1.2-0.20230125001932-da582a95bb8b
+	github.com/google/uuid v1.3.0
 	github.com/joho/godotenv v1.4.0
 	github.com/sethvargo/go-envconfig v0.8.3
+	github.com/sethvargo/go-retry v0.2.4
 	google.golang.org/api v0.103.0
-	google.golang.org/grpc v1.50.1
+	google.golang.org/grpc v1.51.0
 	google.golang.org/protobuf v1.28.1
 )
 
 require (
-	cloud.google.com/go v0.105.0 // indirect
+	cloud.google.com/go v0.107.0 // indirect
 	cloud.google.com/go/compute v1.14.0 // indirect
 	cloud.google.com/go/compute/metadata v0.2.3 // indirect
-	cloud.google.com/go/iam v0.7.0 // indirect
+	cloud.google.com/go/iam v0.8.0 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
-	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
 	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.1 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc v1.0.4 // indirect
+	github.com/lestrrat-go/iter v1.0.2 // indirect
+	github.com/lestrrat-go/jwx/v2 v2.0.6 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
@@ -35,6 +44,7 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20221202195650-67e5cbc046fd // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ integration/main_test.go
@@ -0,0 +1,104 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package integration
+
+import (
+	"bytes"
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"fmt"
+	"net/http"
+	"os"
+	"path"
+	"testing"
+
+	"cloud.google.com/go/bigquery"
+	"github.com/abcxyz/github-metrics-aggregator/pkg/server"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/uuid"
+)
+
+func validateCfg(t *testing.T) *config {
+	t.Helper()
+
+	cfg, err := newTestConfig(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	return cfg
+}
+
+// createSignature creates a HMAC 256 signature for the test request payload.
+func createSignature(key, payload []byte) string {
+	mac := hmac.New(sha256.New, key)
+	mac.Write(payload)
+	return hex.EncodeToString(mac.Sum(nil))
+}
+
+func TestHTTPEndpoints(t *testing.T) {
+	t.Parallel()
+	testutil.SkipIfNotIntegration(t)
+
+	cfg := validateCfg(t)
+	ctx := context.Background()
+	requestID := uuid.New().String()
+
+	resp, err := makeHTTPRequest(requestID, cfg.EndpointURL, cfg)
+	if err != nil {
+		t.Fatalf("error calling service url: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Fatalf("invalid http response code got: %d, want: %d", resp.StatusCode, http.StatusCreated)
+	}
+
+	bqClient := makeBigQueryClient(ctx, t, cfg.ProjectID)
+	query := generateQuery(bqClient, requestID, cfg.ProjectID, cfg.DatasetID)
+	queryIfNumRowsExistWithRetries(ctx, t, query, cfg.QueryRetryWaitDuration, cfg.QueryRetryLimit, "test-main", 1)
+}
+
+func makeHTTPRequest(requestID, endpointURL string, cfg *config) (*http.Response, error) {
+	payload, err := os.ReadFile(path.Join("..", "testdata", "pull_request.json"))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create payload from file: %w", err)
+	}
+
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodPost, endpointURL, bytes.NewReader(payload))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create audit log http request: %w", err)
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", cfg.IDToken))
+	req.Header.Add(server.DeliveryIDHeader, requestID)
+	req.Header.Add(server.EventTypeHeader, "pull_request")
+	req.Header.Add(server.SHA256SignatureHeader, fmt.Sprintf("sha256=%s", createSignature([]byte(cfg.GitHubWebhookSecret), payload)))
+
+	httpClient := &http.Client{Timeout: cfg.HTTPRequestTimeout}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute audit log request: %w", err)
+	}
+	return resp, nil
+}
+
+func generateQuery(bqClient *bigquery.Client, requestID, projectID, datasetID string) *bigquery.Query {
+	queryString := fmt.Sprintf("SELECT COUNT(1) FROM `%s.%s`", projectID, datasetID)
+	queryString += ` WHERE delivery_id = @request_id`
+	return makeQuery(*bqClient, queryString, &[]bigquery.QueryParameter{{Name: "request_id", Value: requestID}})
+}
