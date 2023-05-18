3080fde0da3db798fe767e33052d783719a874b4
Brad Egler <bradegler@google.com>
2023-05-18 14:16:39 -0400

    feat: terraform module updates for leech pipeline

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -335,3 +335,110 @@ resource "google_bigquery_table" "event_views" {
     google_bigquery_table.events_table
   ]
 }
+
+# Leech Table / IAM
+
+resource "google_bigquery_table" "leech_table" {
+  project = data.google_project.default.project_id
+
+  deletion_protection = true
+  table_id            = var.leech_table_id
+  dataset_id          = google_bigquery_dataset.default.dataset_id
+  schema = jsonencode([
+    {
+      "name" : "delivery_id",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "GUID that represents the event that was ingested."
+    },
+    {
+      "name" : "processed_at",
+      "type" : "TIMESTAMP",
+      "mode" : "REQUIRED",
+      "description" : "Timestamp of when the event was processed."
+    },
+    {
+      "name" : "status",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "The status of the log ingestion."
+    },
+    {
+      "name" : "workflow_uri",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "The original workflow uri that trigger the ingestion."
+    },
+    {
+      "name" : "logs_uri",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "The GCS uri of the logs."
+    },
+    {
+      "name" : "github_actor",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "GitHub user that triggered the workflow event."
+    },
+    {
+      "name" : "organization_name",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "GitHub organization name."
+    },
+    {
+      "name" : "repository_name",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "GitHub repository name."
+    },
+    {
+      "name" : "repository_slug",
+      "type" : "STRING",
+      "mode" : "REQUIRED",
+      "description" : "Combined org/repo_name of the repository."
+    },
+  ])
+}
+
+resource "google_bigquery_table_iam_member" "leech_owners" {
+  for_each = toset(var.leech_table_iam.owners)
+
+  project = data.google_project.default.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.leech_table.id
+  role       = "roles/bigquery.dataOwner"
+  member     = each.value
+}
+
+resource "google_bigquery_table_iam_member" "leech_editors" {
+  for_each = toset(var.leech_table_iam.editors)
+
+  project = data.google_project.default.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.leech_table.id
+  role       = "roles/bigquery.dataEditor"
+  member     = each.value
+}
+
+resource "google_bigquery_table_iam_member" "leech_viewers" {
+  for_each = toset(var.leech_table_iam.viewers)
+
+  project = data.google_project.default.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.leech_table.id
+  role       = "roles/bigquery.dataViewer"
+  member     = each.value
+}
+
+resource "google_storage_bucket" "leech_storage_bucket" {
+  name     = var.leech_bucket_name
+  location = var.leech_bucket_location
+
+  uniform_bucket_level_access = true
+  public_access_prevention    = "enforced"
+}
