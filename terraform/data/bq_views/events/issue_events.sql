1445c7b0d829501123c338b0b277b61012603ddb
Brian Bechtel <bbechtel@google.com>
2023-06-23 15:46:23 -0700

    Add views for Issue data
    * Added `issue_events.sql` which creates a view that
      filters events to those only related to Issues.
    * Added `issues.sql` which provides a view of distinct issues.

---
+++ terraform/data/bq_views/events/issue_events.sql
@@ -0,0 +1,53 @@
+-- Copyright 2023 The Authors (see AUTHORS file)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Filters events to those just pertaining to issues.
+-- Relevant GitHub Docs:
+-- https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads#issues
+
+SELECT
+  received,
+  event,
+  JSON_VALUE(payload, "$.action") action,
+  JSON_VALUE(payload, "$.organization.login") organization,
+  SAFE_CAST(JSON_VALUE(payload, "$.organization.id") AS INT64) organization_id,
+  JSON_VALUE(payload, "$.repository.full_name") repository_full_name,
+  SAFE_CAST(JSON_VALUE(payload, "$.repository.id") AS INT64) repository_id,
+  JSON_VALUE(payload, "$.repository.name") repository,
+  JSON_VALUE(payload, "$.repository.visibility") repository_visibility,
+  JSON_VALUE(payload, "$.sender.login") sender,
+  SAFE_CAST(JSON_VALUE(payload, "$.sender.id") AS INT64) sender_id,
+  JSON_VALUE(payload, "$.issue.active_lock_reason") active_lock_reason,
+  JSON_VALUE(payload, "$.issue.assignee.login") assignee,
+  JSON_VALUE(payload, "$.issue.user.login") author,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.user.id") AS INT64) author_id,
+  JSON_VALUE(payload, "$.issue.author_association") author_association,
+  JSON_VALUE(payload, "$.issue.body") body,
+  TIMESTAMP(JSON_VALUE(payload, "$.issue.closed_at")) closed_at,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.comments") AS INT64) comments,
+  TIMESTAMP(JSON_VALUE(payload, "$.issue.created_at")) created_at,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.draft") AS BOOL) draft,
+  JSON_VALUE(payload, "$.issue.html_url") html_url,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.id") AS INT64) id,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.locked") AS BOOL) locked,
+  SAFE_CAST(JSON_VALUE(payload, "$.issue.number") AS INT64) number,
+  TIMESTAMP_DIFF(TIMESTAMP(JSON_VALUE(payload, "$.issue.closed_at")), TIMESTAMP(JSON_VALUE(payload, "$.issue.created_at")), SECOND) open_duration_seconds,
+  JSON_VALUE(payload, "$.issue.state") state,
+  JSON_VALUE(payload, "$.issue.state_reason") state_reason,
+  JSON_VALUE(payload, "$.issue.title") title,
+  TIMESTAMP(JSON_VALUE(payload, "$.issue.updated_at")) updated_at,
+FROM
+  `${dataset_id}.${table_id}`
+WHERE
+  event = "issues";
---
+++ terraform/data/bq_views/resources/issues.sql
@@ -0,0 +1,52 @@
+-- Copyright 2023 The Authors (see AUTHORS file)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Extracts all distinct issues from the issue_events view.
+-- The most recent event for each distinct issue id is used to extract
+-- the issue data. This ensures that the issue data is up to date.
+
+SELECT
+  issue_events.active_lock_reason,
+  issue_events.assignee,
+  issue_events.author,
+  issue_events.author_association,
+  issue_events.body,
+  issue_events.closed_at,
+  issue_events.comments,
+  issue_events.created_at,
+  issue_events.draft,
+  issue_events.html_url,
+  issue_events.id,
+  issue_events.locked,
+  issue_events.number,
+  issue_events.open_duration_seconds,
+  issue_events.organization_id,
+  issue_events.repository_id,
+  issue_events.state,
+  issue_events.state_reason,
+  issue_events.title,
+  issue_events.updated_at,
+FROM
+  `${dataset_id}.${table_id}` issue_events
+INNER JOIN (
+  SELECT
+    id,
+    MAX(received) received
+  FROM
+    `${dataset_id}.${table_id}`
+  GROUP BY
+    id ) unique_issue_ids
+ON
+  issue_events.id = unique_issue_ids.id
+  AND issue_events.received = unique_issue_ids.received;
