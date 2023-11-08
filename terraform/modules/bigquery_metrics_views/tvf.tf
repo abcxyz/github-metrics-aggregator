8cfd0713a9cb0fda886f4827cbb6b69d0a0a63b3
Brian Bechtel <bbechtel@google.com>
2023-11-08 14:23:12 -0800

    Add TVF version of push events view
    * Add a table-valued function version of the push events view to allow for more efficient querying.

--- terraform/modules/bigquery_metrics_views/tvf.tf
+++ terraform/modules/bigquery_metrics_views/tvf.tf
@@ -15,6 +15,32 @@
 # Table Value Functions. Stored here so inputs can be modeled in HCL
 # rather than raw SQL statements.
 
+# Version of push_events that uses TVF
+resource "google_bigquery_routine" "push_events_by_date" {
+  project = var.project_id
+
+  dataset_id   = var.dataset_id
+  routine_id   = "push_events_by_date"
+  routine_type = "TABLE_VALUED_FUNCTION"
+  language     = "SQL"
+  definition_body = templatefile("${path.module}/data/bq_tvf/events/push_events_by_date.sql",
+    {
+      parent_project_id = var.project_id,
+      parent_dataset_id = var.dataset_id,
+      parent_routine_id = var.base_tvf_id,
+    }
+  )
+
+  arguments {
+    name      = "startTimestamp"
+    data_type = jsonencode({ typeKind : "TIMESTAMP" })
+  }
+  arguments {
+    name      = "endTimestamp"
+    data_type = jsonencode({ typeKind : "TIMESTAMP" })
+  }
+}
+
 # Version of pull_request_events that uses TVF
 resource "google_bigquery_routine" "pull_request_events_by_date" {
   project = var.project_id
