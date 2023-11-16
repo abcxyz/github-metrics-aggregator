8a1e82c999e6d04fd5f02a7c6aa2deced653d0bf
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2023-11-15 17:25:16 -0800

    chore: add initial infra template (#189)

---
+++ abc.templates/infra/contents/main.tf
@@ -0,0 +1,43 @@
+locals {
+  project_id                        = "REPLACE_PROJECT_ID"
+  automation_service_account_member = "REPLACE_AUTOMATION_SERVICE_ACCOUNT_MEMBER"
+  leech_bucket_name                 = "REPLACE_LEECH_BUCKET_NAME"
+  leech_bucket_location             = "REPLACE_LEECH_BUCKET_LOCATION"
+}
+
+module "REPLACE_MODULE_NAME" {
+  source = "git::https://github.com/abcxyz/github-metrics-aggregator.git//terraform?ref=v0.0.2"
+
+  project_id = local.project_id
+
+  image                             = "gcr.io/cloudrun/placeholder@sha256:f1586972ac147796d60ee2c5a0d6cc78067fc862b6d715d6d2a96826455c3423"
+  prefix_name                       = "REPLACE_CUSTOM_NAME"
+  bigquery_project_id               = local.project_id
+  automation_service_account_member = local.automation_service_account_member
+  webhook_domains                   = ["REPLACE_DOMAIN"]
+  github_app_id                     = "REPLACE_GITHUB_APP_ID"
+  log_level                         = "info"
+  retry_service_iam = {
+    admins     = []
+    developers = []
+    invokers   = []
+  }
+  webhook_service_iam = {
+    admins     = []
+    developers = []
+    invokers   = ["allUsers"] # public access, called by github webhook
+  }
+  dataset_iam = {
+    owners  = []
+    editors = []
+    viewers = []
+  }
+  leech_table_iam = {
+    owners  = []
+    editors = []
+    viewers = []
+  }
+  # TODO (jonathanhong): move this out of the core module
+  leech_bucket_name     = local.leech_bucket_name
+  leech_bucket_location = local.leech_bucket_location
+}
