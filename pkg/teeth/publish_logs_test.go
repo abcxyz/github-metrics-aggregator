c0e74d6ec7e352d6b9028d5a61da5a257da75724
Stanley Chen <stanchen@google.com>
2023-11-20 22:06:39 +0000

    fix linter errors

--- pkg/teeth/publish_logs_test.go
+++ pkg/teeth/publish_logs_test.go
@@ -1,3 +1,17 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package teeth
 
 import (
@@ -8,6 +22,7 @@ import (
 )
 
 func TestSourceQuery(t *testing.T) {
+	t.Parallel()
 	const (
 		testPullRequestEventsTable = "github_metrics_aggregator.pull_request_events"
 		testEventsTable            = "github_metrics_aggregator.events"
