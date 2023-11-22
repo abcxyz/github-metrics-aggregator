e51090d00d3d77d1605716064df4bd76ab1732f5
pdewilde <pdewilde@google.com>
2023-11-21 16:34:23 -0800

    WIP sanitizer

---
+++ pkg/bigquery/sanitize.go
@@ -0,0 +1,83 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package bigquery implements helpers for using BigQuery.
+package bigquery
+
+import "unicode/utf8"
+
+// Start with lowercase, middle is lowercase, number or hyphen, cannot end in
+// hyphen. 6-30 characters in length (start, 4-28 middle, end).
+const projectIDRegex = `^[a-z][a-z0-9\-]{4,28}[a-z0-9]$`
+
+// Lowercase and uppercase letters and underscores. Max 1024 characters.
+const datasetIDRegex = `^[a-zA-Z0-9_]{1,1024}$`
+
+// Unicode characters in category L (letter), M (mark), N (number),
+// Pc (connector, including underscore), Pd (dash), Zs (space). Max 1024
+// characters. 1024 is actually UTF-8 bytes from experimentation.
+// UTF-8 length check will be done separately.
+const tableNameRegex = `^[\p{L}\p{M}\p{N}\p{Pc}\p{Pd}\p{Zs}]{1,1024}$`
+
+// Returns nil if provided string is a valid GCP project id, else returns an
+// error. Based on rules defined in
+// https://cloud.google.com/resource-manager/docs/creating-managing-projects.
+// Should allow for safe string interpolation without SQL injection, as
+// identifiers cannot be replaced via parameters in BigQuery.
+// In most cases, setting the default GCP project in the client's Query object
+// is preferable as it avoids the injection risk entirely.
+// Does not check for restricted strings such as google, null, ect.
+func validateGCPProjectID(projectID string) error {
+
+	// todo: merr
+	if len(projectID) > 30 || len(projectID) < 6 {
+		// TODO: error
+	}
+
+}
+
+// Returns nil if provided string is a valid dataset id, else returns an
+// error. Based on rules defined in
+// https://cloud.google.com/bigquery/docs/datasets#dataset-naming.
+// Should allow for safe string interpolation without SQL injection, as
+// identifiers cannot be replaced via parameters in BigQuery.
+// In most cases, setting the default dataset in the client's Query object
+// is preferable as it avoids the injection risk entirely.
+// Does not check for restricted strings such as google, null, ect.
+func validateDatasetID(datasetID string) error {
+
+}
+
+// Returns nil if provided string is a valid table name, else returns an
+// error. Based on rules defined in
+// https://cloud.google.com/bigquery/docs/tables#table_naming.
+// Should allow for safe string interpolation without SQL injection, as
+// identifiers cannot be replaced via parameters in BigQuery.
+// Hardcoding table names may be preferred to avoid any injection risk.
+// Unclear if these rules are valid for all external tables.
+// Does not check for restricted strings such as google, null, ect.
+func validateTableName(tableName string) error {
+	// TODO: is this really necessary, seems like regexp may do this for free.
+	if !utf8.Valid([]byte(tableName)) {
+
+	}
+	// Checking to ensure max 1024 UTF-8 bytes, as that is the limit actually
+	// in place.
+	if len(tableName) > 1024 {
+
+	}
+	// Regex has some length validation, though only lower bound should ever be
+	// hit.
+	// TODO: regex
+}
