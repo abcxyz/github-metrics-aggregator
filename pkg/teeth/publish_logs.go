1be3239f8fd7a73821cf73cd83ac9e6cedbac2f2
Stanley Chen <stanchen@google.com>
2023-12-01 01:09:52 +0000

    Check in source query and create folder for pipeline (#194)
    The new pipeline is called `teeth` because it rhymes with `leech` and
    leeches have teeth ✌️

---
+++ pkg/teeth/publish_logs.go
@@ -0,0 +1,91 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package teeth contains a job that will read leech and pull request event
+// records from BigQuery and publish any available log invocations in a PR
+// comment.
+package teeth
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"text/template"
+	"time"
+
+	"cloud.google.com/go/bigquery"
+
+	_ "embed"
+)
+
+// PublisherSourceQuery is the source query that teeth job pipeline
+// will use to publish results.
+//
+//go:embed sql/publisher_source.sql
+var PublisherSourceQuery string
+
+// BigQueryClient defines the spec for calls to read from and write to
+// BigQuery tables.
+type BigQueryClient interface {
+	Config() *BQConfig
+	Query(string) *bigquery.Query
+}
+
+// TODO: Add query limit param.
+//
+// BQConfig defines configuration parameters for the BigQuery tables
+// used by the teeth job pipeline.
+type BQConfig struct {
+	PullRequestEventsTable       string
+	InvocationCommentStatusTable string
+	EventsTable                  string
+	LeechStatusTable             string
+}
+
+// PublisherSourceRecord maps the columns from the driving BigQuery query
+// to a usable structure.
+type PublisherSourceRecord struct {
+	DeliveryID     string    `bigquery:"delivery_id"`
+	PullRequestID  int       `bigquery:"pull_request_id"`
+	PullRequestURL string    `bigquery:"pull_request_html_url"`
+	Received       time.Time `bigquery:"received"`
+	LogsURI        string    `bigquery:"logs_uri"`
+	HeadSHA        string    `bigquery:"head_sha"`
+}
+
+// InvocationCommentStatusRecord is the output data structure that maps to the
+// teeth pipeline's output table schema for invocation comment statuses.
+type InvocationCommentStatusRecord struct {
+	PullRequestID  int                `bigquery:"pull_request_id"`
+	PullRequestURL string             `bigquery:"pull_request_html_url"`
+	ProcessedAt    time.Time          `bigquery:"processed_at"`
+	CommentID      bigquery.NullInt64 `bigquery:"comment_id"`
+	Status         string             `bigquery:"status"`
+	JobName        string             `bigquery:"job_name"`
+}
+
+// SetUpPublisherSourceQuery converts the PublisherSourceQuery string into a
+// Query object that the BigQueryClient can then execute. It populates the
+// query parameters with the BigQuery config values.
+func SetUpPublisherSourceQuery(ctx context.Context, bqClient BigQueryClient) (*bigquery.Query, error) {
+	tmpl, err := template.New("publisher").Parse(PublisherSourceQuery)
+	if err != nil {
+		return nil, fmt.Errorf("failed to set up sql template: %w", err)
+	}
+	var b bytes.Buffer
+	if err = tmpl.Execute(&b, bqClient.Config()); err != nil {
+		return nil, fmt.Errorf("failed to execute sql template: %w", err)
+	}
+	return bqClient.Query(b.String()), nil
+}
---
+++ pkg/teeth/sql/publisher_source.sql
@@ -0,0 +1,55 @@
+-- Copyright 2023 The Authors (see AUTHORS file)
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+SELECT
+  pull_request_events.delivery_id,
+  delivery_events.pull_request_id,
+  html_url AS pull_request_html_url,
+  delivery_events.received,
+  logs_uri,
+  head_sha
+FROM
+  `{{.PullRequestEventsTable}}` AS pull_request_events
+JOIN (
+  SELECT
+    delivery_id,
+    received,
+    logs_uri,
+    SAFE.INT64(pull_request.id) AS pull_request_id,
+    LAX_STRING(pull_request.url) AS pull_request_url,
+    LAX_STRING(events.payload.workflow_run.head_sha) AS head_sha,
+  FROM
+    `{{.LeechStatusTable}}` leech_status
+  JOIN (
+	  SELECT
+      *
+    FROM
+      `{{.EventsTable}}` events,
+      UNNEST(JSON_EXTRACT_ARRAY(events.payload.workflow_run.pull_requests)) AS pull_request
+    WHERE
+      received >= TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL -30 DAY)) AS events
+  USING
+    (delivery_id)) AS delivery_events
+ON
+  pull_request_events.id = delivery_events.pull_request_id
+WHERE
+  pull_request_events.id NOT IN (
+  SELECT
+    DISTINCT pull_request_id
+  FROM
+    `{{.InvocationCommentStatusTable}}` invocation_comment_status)
+  AND merged_at BETWEEN TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL -30 DAY)
+  AND TIMESTAMP_ADD(CURRENT_TIMESTAMP(), INTERVAL -1 HOUR)
+ORDER BY
+  received, 
+  pull_request_events.id ASC
