9e465a3e4f340dfbf46d88bc557600ff93a91512
Jonathan Hong <119632588+gjonathanhong@users.noreply.github.com>
2024-01-10 13:10:59 -0800

    Remove predefined roles and upgrade template version to v0.0.15 (#226)

--- abc.templates/infra/contents/main.tf
+++ abc.templates/infra/contents/main.tf
@@ -4,12 +4,11 @@ locals {
 }
 
 module "REPLACE_MODULE_NAME" {
-  source = "git::https://github.com/abcxyz/github-metrics-aggregator.git//terraform?ref=v0.0.14"
+  source = "git::https://github.com/abcxyz/github-metrics-aggregator.git//terraform?ref=v0.0.15"
 
   project_id = local.project_id
 
   image                             = "gcr.io/cloudrun/placeholder@sha256:f1586972ac147796d60ee2c5a0d6cc78067fc862b6d715d6d2a96826455c3423"
-  prefix_name                       = "REPLACE_CUSTOM_NAME"
   bigquery_project_id               = local.project_id
   automation_service_account_member = local.automation_service_account_member
   webhook_domains                   = ["REPLACE_DOMAIN"]
@@ -30,8 +29,4 @@ module "REPLACE_MODULE_NAME" {
     editors = []
     viewers = []
   }
-
-  depends_on = [
-    google_project_iam_member.REPLACE_MODULE_NAME_actuators
-  ]
 }
