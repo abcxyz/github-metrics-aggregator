f63a2734fdd182b7cac7959c986cec62365abe5e
maleo <maleo@google.com>
2024-05-02 14:08:58 +0000

    Retag existing image instead of building image without additional layers
    which is more efficient
    
    Signed-off-by: maleo <maleo@google.com>

--- abc.templates/deployments/contents/deploy.sh
+++ abc.templates/deployments/contents/deploy.sh
@@ -3,12 +3,12 @@ set -euo pipefail
 
 GITHUB_METRICS_VERSION='REPLACE_GITHUB_METRICS_VERSION_TAG'
 PLATFORM='amd64'
+VERSION="${GITHUB_METRICS_VERSION}-${PLATFORM}"
+UPSTREAM_IMAGE_NAME="us-docker.pkg.dev/abcxyz-artifacts/docker-images/github-metrics-aggregator:${VERSION}"
 IMAGE_NAME="REPLACE_FULL_IMAGE_NAME:${GITHUB_METRICS_VERSION}-${GITHUB_SHA}"
 
-docker build -t "${IMAGE_NAME}" \
-  --build-arg="VERSION=${GITHUB_METRICS_VERSION}-${PLATFORM}" \
-  REPLACE_SUBDIRECTORY
-
+docker pull "${UPSTREAM_IMAGE_NAME}"
+docker tag "${UPSTREAM_IMAGE_NAME}" "${IMAGE_NAME}"
 docker push "${IMAGE_NAME}"
 
 gcloud run services update "${WEBHOOK_SERVICE_NAME}" \
