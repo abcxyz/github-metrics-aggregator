9087bf41320a99a70ddd484e00352dafae848b99
Brian Bechtel <lock14@users.noreply.github.com>
2024-07-15 09:29:08 -0700

    Update pkg/review/breakglass_query.go
    Co-authored-by: Seth Vargo <seth@sethvargo.com>
    Signed-off-by: Brian Bechtel <lock14@users.noreply.github.com>

--- pkg/review/breakglass_query.go
+++ pkg/review/breakglass_query.go
@@ -46,7 +46,7 @@ type bgQueryParameters struct {
 
 // makeBreakglassQuery returns a BigQuery query that searches for a break glass
 // issue created by given user and within a specified time frame.
-func makeBreakglassQuery(cfg *Config, author string, timestamp time.Time) (string, error) {
+func makeBreakglassQuery(cfg *Config, author string, timestamp *time.Time) (string, error) {
 	tmpl, err := template.New("breakglass-query").Parse(breakGlassIssueSQL)
 	if err != nil {
 		return "", fmt.Errorf("failed to parse query template: %w", err)
