b62787231ed939391cb09d918cd58c68ea2785fd
Shan Kiyani <shankiyani@google.com>
2024-09-12 12:40:51 -0400

    remove existing alerts

--- terraform/modules/alerts/notifications.tf
+++
@@ -1,9 +0,0 @@
-resource "google_monitoring_notification_channel" "non_paging" {
-  for_each = var.alert_notification_channel_non_paging
-
-  project = var.project_id
-
-  display_name = "Non-paging Notification Channel"
-  type         = each.key
-  labels       = each.value.labels
-}
