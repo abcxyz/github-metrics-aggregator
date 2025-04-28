31f7f619ffe84a632cfce8594c4c6b6b8807a7f9
Shan Kiyani <shankiyani@google.com>
2025-04-28 13:44:09 -0400

    only provision notification channels if provided (#375)

--- terraform/notification.tf
+++ terraform/notification.tf
@@ -1,5 +1,8 @@
 resource "google_monitoring_notification_channel" "non_paging" {
-  for_each = var.alert_notification_channel_non_paging
+  for_each = {
+    for k, v in var.alert_notification_channel_non_paging : k => v
+    if k == "email" && try(v.labels.email_address, "") != ""
+  }
 
   project = var.project_id
 
