d4aef0de7188b819e5109b0c7c429e0f8d30f671
Jessika Duquette <jessikad@google.com>
2026-01-27 17:22:32 +0000

    Creating new table with relay subscription

--- terraform/modules/bigquery_infra/main.tf
+++ terraform/modules/bigquery_infra/main.tf
@@ -184,6 +184,132 @@ resource "google_bigquery_table_iam_member" "event_viewers" {
   member     = each.value
 }
 
+# Optimized Events table with clustering and partitioning / IAM
+resource "google_bigquery_table" "optimized_events_table" {
+  project = var.project_id
+
+  deletion_protection = var.deletion_protection
+  table_id            = "optimized_events"
+  dataset_id          = google_bigquery_dataset.default.dataset_id
+  schema = jsonencode([
+    {
+      "name" : "delivery_id",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "GUID from the GitHub webhook header (X-GitHub-Delivery)"
+    },
+    {
+      "name" : "signature",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Signature from the GitHub webhook header (X-Hub-Signature-256)"
+    },
+    {
+      "name" : "received",
+      "type" : "TIMESTAMP",
+      "mode" : "NULLABLE",
+      "description" : "Timestamp for when an event is received"
+    },
+    {
+      "name" : "event",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Event type from GitHub webhook header (X-GitHub-Event)"
+    },
+    {
+      "name" : "enterprise_name",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Enterprise name from payload"
+    },
+    {
+      "name" : "enterprise_id",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Enterprise ID from payload"
+    },
+    {
+      "name" : "organization_name",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Organization login from payload"
+    },
+    {
+      "name" : "organization_id",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Organization ID from payload"
+    },
+    {
+      "name" : "repository_name",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Repository name from payload"
+    },
+    {
+      "name" : "repository_id",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Repository ID from payload"
+    },
+    {
+      "name" : "payload",
+      "type" : "STRING",
+      "mode" : "NULLABLE",
+      "description" : "Event payload JSON string"
+    }
+  ])
+
+  time_partitioning {
+    field = "received"
+    type  = var.bigquery_events_partition_granularity
+  }
+
+  clustering = ["event", "enterprise_name", "organization_name", "repository_name"]
+}
+
+resource "google_bigquery_table_iam_member" "optimized_event_owners" {
+  for_each = toset(var.events_table_iam.owners)
+
+  project = var.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.optimized_events_table.id
+  role       = "roles/bigquery.dataOwner"
+  member     = each.value
+}
+
+resource "google_bigquery_table_iam_member" "optimized_event_editors" {
+  for_each = toset(var.events_table_iam.editors)
+
+  project = var.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.optimized_events_table.id
+  role       = "roles/bigquery.dataEditor"
+  member     = each.value
+}
+
+resource "google_bigquery_table_iam_member" "optimized_event_viewers" {
+  for_each = toset(var.events_table_iam.viewers)
+
+  project = var.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.optimized_events_table.id
+  role       = "roles/bigquery.dataViewer"
+  member     = each.value
+}
+
+resource "google_bigquery_table_iam_member" "optimized_event_pubsub_agent_editor" {
+  project = var.project_id
+
+  dataset_id = google_bigquery_dataset.default.dataset_id
+  table_id   = google_bigquery_table.optimized_events_table.id
+  role       = "roles/bigquery.dataEditor"
+  member     = "serviceAccount:service-${data.google_project.default.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
+}
+
 # Events table with clustering and partitioning / IAM
 resource "google_bigquery_table" "raw_events_table" {
   project = var.project_id
--- terraform/modules/bigquery_infra/outputs.tf
+++ terraform/modules/bigquery_infra/outputs.tf
@@ -1,17 +1,3 @@
-# Copyright 2023 The Authors (see AUTHORS file)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 output "dataset_id" {
   description = "The ID of the BigQuery dataset."
   value       = google_bigquery_dataset.default.dataset_id
@@ -25,6 +11,11 @@ output "events_table_id" {
 output "raw_events_table_id" {
   description = "The ID of the BigQuery table for raw events."
   value       = google_bigquery_table.raw_events_table.table_id
+}
+
+output "optimized_events_table_id" {
+  description = "The ID of the BigQuery table for optimized events."
+  value       = google_bigquery_table.optimized_events_table.table_id
 }
 
 output "checkpoint_table_id" {
